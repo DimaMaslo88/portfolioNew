620f64eac9a1219fc56547337ee09469440cc48f
DimaMaslo88 <dmitrij-maslo@mail.ru>
2022-05-17 22:33:00 +0300

    "Portfolio"

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -6,6 +6,11 @@ import {Title} from "../common/components/title/Title";
 import {faJs} from "@fortawesome/free-brands-svg-icons/faJs";
 import {faAtom} from "@fortawesome/free-solid-svg-icons/faAtom";
 import {faCss3} from "@fortawesome/free-brands-svg-icons/faCss3";
+import {faServer} from "@fortawesome/free-solid-svg-icons/faServer";
+import {faGit} from "@fortawesome/free-brands-svg-icons/faGit";
+import {faArrowRightFromBracket} from "@fortawesome/free-solid-svg-icons/faArrowRightFromBracket";
+import {faRightFromBracket} from "@fortawesome/free-solid-svg-icons/faRightFromBracket";
+import {faLaptop} from "@fortawesome/free-solid-svg-icons/faLaptop";
 
 
 export const Skills = () => {
@@ -24,12 +29,27 @@ export const Skills = () => {
                     <Skill
                         icon={faCss3}
                         title={'CSS'}
-                           description={'Good knowledges'}
+                        description={'Good knowledges'}
                     />
                     <Skill
                         icon={faAtom}
                         title={'React'}
-                           description={'Working with React-Redux.Using Thunk.Deploy progress into GitHub Page '}
+                        description={'Working with React-Redux.Deploy progress into GitHub Page '}
+                    />
+                    <Skill
+                        icon={faServer}
+                        title={'Rest-API'}
+                        description={'Handling server requests'}
+                    />
+                    <Skill
+                        icon={faGit}
+                        title={'GitHub'}
+                        description={'Ability to work with git,uploading projects...'}
+                    />
+                    <Skill
+                        icon={faLaptop}
+                        title={'TypeScript'}
+                        description={'All projects are written in typescript'}
                     />
 
 
