6e630670724af2a0e897c5c207f5c33983090d65
DimaMaslo88 <dmitrij-maslo@mail.ru>
2022-07-03 11:11:52 +0300

    "Portfolio"

--- src/main_content/Main_Content.js
+++ src/main_content/Main_Content.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import style from './Main_Content.module.scss'
 import styleContainer from '../common/styles/Container.module.css'
-// import {Parallax,Background} from "react-parallax/@types/index";
 import Slide from 'react-reveal/Slide';
-
+import ReactTypingEffect from 'react-typing-effect';
+import Tilt from 'react-tilt'
 export const MainContent = () => {
     return (
 
         <div className={style.mainBlock}>
-            {/*<Parallax strength={300}>*/}
-            {/*    <Background className="custom-bg">*/}
-            {/*        <img src="http://www.fillmurray.com/500/320" alt="fill murray" />*/}
-            {/*    </Background>*/}
-            {/*</Parallax>*/}
+
 
             <div className={styleContainer.container}>
                 <Slide left>
@@ -20,17 +16,23 @@ export const MainContent = () => {
 
                     <span className={style.span}>Hi Friends</span>
                     <h2 className={style.h2}>{"I`m Dmitry Maslo"}</h2>
-                    <p className={style.p}>React/Redux Frontend developer</p>
+                   {/*<p className={style.p}>React/Redux Frontend developer</p>*/}
+                    <ReactTypingEffect
 
+                        text={'React/Redux Frontend developer'}
+                    />
 
                 </div>
                 </Slide>
                 <Slide right>
-                <div className={style.foto}>
-                    <div className={style.ava}>
-                    </div>
+                    <Tilt className="Tilt" options={{ max : 25 }}  >
+                        <div className={style.foto}>
+                            <div className={style.ava}>
+                            </div>
 
-                </div>
+                        </div>
+                    </Tilt>
+
                 </Slide>
             </div>
 
